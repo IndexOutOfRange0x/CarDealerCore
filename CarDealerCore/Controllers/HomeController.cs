2b484c848579033e1b0fae56f8b001cbb07b9b67
Serik Maratov <serikssau@gmail.com>
2021-05-28 13:29:37 +0500

    added views for web-output

--- CarDealerCore/Controllers/HomeController.cs
+++ CarDealerCore/Controllers/HomeController.cs
@@ -1,12 +1,72 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CarDealerCore.Models;
 
 namespace CarDealerCore.Controllers
 {
     public class HomeController : Controller
     {
+    
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        
         public IActionResult Index()
         {
             return View();
+        }
+        
+        public IActionResult Car()
+        {
+            return View();
+        }
+    
+        public IActionResult MySales()
+        {
+            return View();
+        }
+        
+        public IActionResult AllSales()
+        {
+            return View();
+        }
+    
+        public IActionResult UserPage()
+        {
+            return View();
+        }
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult AddCar()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Buy()
+        {
+            return RedirectToAction("MySales");
+        }
+        
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            ViewData["statusCode"] = HttpContext.Request.Query["code"];
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
         }
     }
 }
